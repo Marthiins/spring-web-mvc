62d81fc0093c3d7077db69911ef7f45374005d7f
Marthiins <sergio.marthiins@gmail.com>
2022-03-20 15:30:59 -0300

    Carregando nossa lista de pessoas cadastradas

--- springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -4,24 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
 
 @Controller
 public class PessoaController {
-	
 
 	@Autowired
-	private PessoaRepository pessoaRepository;/*Injeção de dependencia pessoa da classe repository*/
-	
+	private PessoaRepository pessoaRepository;/* Injeção de dependencia pessoa da classe repository */
+
 	/*--------Inicio da Tela------------*/
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public String inicio() {
 		return "cadastro/cadastropessoa";
 	}
-	
+
 	/*--------Metodo salvar e voltar na mesma tela------------*/
 
 	@RequestMapping(method = RequestMethod.POST, value = "/salvarpessoa")
@@ -30,4 +30,11 @@ public class PessoaController {
 		return "cadastro/cadastropessoa";
 	}
 
+	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
+	public ModelAndView pessoas() {
+		ModelAndView andview = new ModelAndView("cadastro/cadastropessoa");/* Instanciar o objeto */
+		Iterable<Pessoa> pessoasIterable = pessoaRepository.findAll(); /* Carregar do banco de dados a minha lista */
+		andview.addObject("pessoas", pessoasIterable);/* Adicionar a lista */
+		return andview;
+	}
 }
